bf2f3037d72846bcfeb357d78f14df64e9bcb3ae
José Otavio Carvalho <carvalho.jose@gmail.com>
2025-09-11 17:30:58 -0300

    change animated sidebar

--- src/components/layout/app-shell.tsx
+++ src/components/layout/app-shell.tsx
@@ -18,23 +18,18 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import {
 	Sidebar,
-	SidebarContent,
-	SidebarFooter,
-	SidebarHeader,
-	SidebarInset,
-	SidebarMenu,
-	SidebarMenuButton,
-	SidebarMenuItem,
+	SidebarBody,
+	SidebarLink,
 	SidebarProvider,
-	SidebarTrigger,
 } from "@/components/ui/sidebar";
 import { useData } from "@/context/data-context";
-import { slugify } from "@/lib/utils";
+import { cn, slugify } from "@/lib/utils";
 import { Logo } from "../icons/logo";
 import { Button } from "../ui/button";
 import { DropdownMenu } from "../ui/dropdown-menu";
 import { LoadingSpinner } from "../ui/loading-spinner";
 import { useSidebar } from "../ui/sidebar";
+import { LogoMobile } from "../icons/logo-mobile";
 
 export function AppLayoutWithSidebarProvider({
 	children,
@@ -64,7 +59,7 @@ function AppLayout({ children }: { children: React.ReactNode }) {
 	const pathname = usePathname();
 	const { isSignedIn, isLoaded, has } = useAuth();
 	const { user } = useUser();
-	const { setOpenMobile, state, isMobile } = useSidebar();
+	const { setOpen, open } = useSidebar();
 	const { loadingData, atletas } = useData();
 	const [showScrollToTop, setShowScrollToTop] = useState(false);
 
@@ -93,15 +88,15 @@ function AppLayout({ children }: { children: React.ReactNode }) {
 	}, []);
 
 	const menuItems = [
-		{ href: "/eventos", label: "Calendário", icon: Calendar },
-		{ href: "/rankings", label: "Rankings", icon: BarChart },
-		{ href: "/atletas", label: "Atletas", icon: Users },
-		{ href: "/fotos", label: "Fotos", icon: ImageIcon },
-		{ href: "/videos", label: "Vídeos", icon: Video },
-		{ href: "/historia", label: "História", icon: BookOpen },
-		{ href: "/contato", label: "Contato", icon: Mail },
-		{ href: "/bodyboard", label: "Sobre o Bodyboard", icon: Waves },
-		{ href: "/patrocinio", label: "Patrocínios", icon: Megaphone },
+		{ href: "/eventos", label: "Calendário", icon: <Calendar /> },
+		{ href: "/rankings", label: "Rankings", icon: <BarChart /> },
+		{ href: "/atletas", label: "Atletas", icon: <Users /> },
+		{ href: "/fotos", label: "Fotos", icon: <ImageIcon /> },
+		{ href: "/videos", label: "Vídeos", icon: <Video /> },
+		{ href: "/historia", label: "História", icon: <BookOpen /> },
+		{ href: "/contato", label: "Contato", icon: <Mail /> },
+		{ href: "/bodyboard", label: "Sobre o Bodyboard", icon: <Waves /> },
+		{ href: "/patrocinio", label: "Patrocínios", icon: <Megaphone /> },
 	];
 
 	if (!isLoaded || loadingData) {
@@ -114,37 +109,20 @@ function AppLayout({ children }: { children: React.ReactNode }) {
 	const canEdit = isSignedIn && userEmail && atleta?.email === userEmail;
 
 	return (
-		<>
-			<Sidebar>
-				<SidebarHeader>
-					<Link
-						href="/"
-						className="flex items-center gap-2"
-						onClick={() => setOpenMobile(false)}
-					>
-						<Logo />
-					</Link>
-				</SidebarHeader>
-				<SidebarContent>
-					<SidebarMenu>
-						{menuItems.map((item) => (
-							<SidebarMenuItem key={item.label}>
-								<SidebarMenuButton
-									asChild
-									isActive={pathname === item.href}
-									tooltip={item.label}
-									onClick={() => setOpenMobile(false)}
-								>
-									<Link href={item.href}>
-										<item.icon />
-										<span>{item.label}</span>
-									</Link>
-								</SidebarMenuButton>
-							</SidebarMenuItem>
+		<div
+			className={cn(
+				"mx-auto flex w-full max-w-7xl flex-1 flex-col overflow-hidden rounded-md border border-neutral-200 bg-gray-100 md:flex-row ",
+				"", // for your use case, use `h-screen` instead of `h-[60vh]` dark:border-neutral-700 dark:bg-neutral-800
+			)}
+		>
+			<Sidebar open={open} setOpen={setOpen} animate>
+				<SidebarBody className="justify-between gap-10 fixed top-0 left-0 h-screen overflow-x-hidden overflow-y-auto z-30">
+					<div>
+						{open ? <SidebarLink link={{ href: "/", label: "", icon: <Logo /> }} /> : <SidebarLink link={{ href: "/", label: "", icon: <LogoMobile /> }} />}
+						{menuItems.map((item, idx) => (
+							<SidebarLink key={idx} link={item} />
 						))}
-					</SidebarMenu>
-				</SidebarContent>
-				<SidebarFooter>
+					</div>
 					<DropdownMenu>
 						<UserButton>
 							<UserButton.MenuItems>
@@ -158,14 +136,15 @@ function AppLayout({ children }: { children: React.ReactNode }) {
 							</UserButton.MenuItems>
 						</UserButton>
 					</DropdownMenu>
-				</SidebarFooter>
+				</SidebarBody>
 			</Sidebar>
-			<SidebarInset
-				className={`${isMobile ? "w-full" : state === "expanded" ? "w-[min(calc(100%-14.5rem),72rem)] max-w-[calc(100%-16rem)]" : "w-full"}`}
+			{/* "relative flex min-h-svh w-full flex-1 flex-col ",
+			"peer-data-[variant=inset]:min-h-[calc(100svh-theme(spacing.4))] md:peer-data-[variant=inset]:m-2 md:peer-data-[state=collapsed]:peer-data-[variant=inset]:ml-2 md:peer-data-[variant=inset]:ml-0 md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow" */}
+			<div
+				className={`flex min-h-svh w-full flex-1 flex-col transition-all duration-300 ease-in-out ${open ? "w-full md:w-[calc(100%-300px)] md:ml-[300px]" : "w-full"}`}
 			>
-				<header className="sticky top-0 z-10 backdrop-blur w-full flex items-center justify-between p-4 border-b">
+				<header className={`fixed top-10 md:top-0 z-20 backdrop-blur flex items-center justify-between p-4 border-b bg-white/80 transition-all duration-300 ${open ? 'left-0 md:left-[300px] w-full md:w-[calc(100%-300px)]' : 'left-0 md:left-[60px] w-full md:w-[calc(100%-60px)]'}`}>
 					<div className="flex items-center gap-2">
-						<SidebarTrigger />
 						<h2 className="font-headline text-base md:text-lg font-semibold capitalize">
 							{pathname.split("/").pop()?.replace(/-/g, " ") || "Home"}
 						</h2>
@@ -186,11 +165,11 @@ function AppLayout({ children }: { children: React.ReactNode }) {
 						<SignInButton>Entrar</SignInButton>
 					)}
 				</header>
-				<main className={`p-4 md:p-6 lg:p-8 ${isMobile ? 'pt-10' : 'pt-20'} pb-20 relative`}>
+				<main className={`p-4 md:p-6 lg:p-8 ${open ? 'ml-0' : 'ml-0 md:ml-[60px]'} pt-28 md:pt-24 pb-20 relative`}>
 					{children}
 					<Button
 						onClick={scrollToTop}
-						className={`fixed bottom-6 right-6 z-50 rounded-full w-12 h-12 shadow-lg hover:shadow-xl transition-all duration-600 bg-primary hover:bg-primary/80 text-white ${showScrollToTop
+						className={`fixed bottom-6 right-6 z-50 rounded-full w-12 h-12 shadow-lg hover:shadow-xl transition-all duration-300 bg-primary hover:bg-primary/80 text-white ${showScrollToTop
 							? 'opacity-100 translate-y-0 pointer-events-auto'
 							: 'opacity-0 translate-y-2 pointer-events-none'
 							}`}
@@ -200,7 +179,7 @@ function AppLayout({ children }: { children: React.ReactNode }) {
 						<ChevronUp className="w-5 h-5" />
 					</Button>
 				</main>
-			</SidebarInset>
-		</>
+			</div>
+		</div>
 	);
 }
